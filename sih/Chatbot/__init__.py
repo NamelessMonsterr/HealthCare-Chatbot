51c14ce3f35aaeb879b27807a70cdf1ce451ef58
NamelessMonsterr <168415171+NamelessMonsterr@users.noreply.github.com>
2025-09-05 16:33:07 +0530

    Update __init__.py

--- sih/Chatbot/__init__.py
+++ sih/Chatbot/__init__.py
@@ -5,21 +5,37 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['WHOOSH_BASE'] = 'whoosh'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'aditya@gmail.com'
-app.config['MAIL_PASSWORD'] = 'adi1234'
-mail = Mail(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+mail = Mail()
 
-from Chatbot import routes
+def create_app():
+    app = Flask(__name__)
+
+    # Load config - replace with your actual config or from environment variables
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['WHOOSH_BASE'] = 'whoosh'
+
+    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'aditya@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'adi1234'
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
+
+    mail.init_app(app)
+
+    # Import and register blueprints here
+    from Chatbot.routes import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
